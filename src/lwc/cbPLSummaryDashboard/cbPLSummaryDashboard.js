289a47293006e2d819c793036817879cbec6d663
CloudBudget Inc <68500411+CloudBudgetInc@users.noreply.github.com>
2024-07-01 11:14:22 +0200

    COGS Chart added

--- src/lwc/cbPLSummaryDashboard/cbPLSummaryDashboard.js
+++ src/lwc/cbPLSummaryDashboard/cbPLSummaryDashboard.js
@@ -1,29 +1,30 @@
 import {api, LightningElement, track} from 'lwc';
 import {prepareRevenue} from "./cbPLSummaryDashboardRevenue";
+import {prepareCOGS} from "./cbPLSummaryDashboardCOGS";
+import {_message} from "c/cbUtils";
 
 export default class CbPLSummaryDashboard extends LightningElement {
 
 	@api reportLines;
 	@api closeFunction;
-	@track readyToRenderRevenue = false;
 
 	connectedCallback() {
 		try {
-			console.log(this.reportLines);
 			if (!this.reportLines || this.reportLines.length === 0) {
-				alert('Nothing to render');
+				_message('info', 'Nothing to render');
 				return null;
 			}
 			this.reportLines = JSON.parse(this.reportLines);
 			this.prepareData();
 		} catch (e) {
-			alert('CALLBACK ERROR :  ' + JSON.stringify(e));
+			_message('error', 'CALLBACK ERROR :  ' + JSON.stringify(e));
 		}
 	}
 
 	prepareData = () => {
 		this.readyToRenderRevenue = false;
 		prepareRevenue(this);
+		prepareCOGS(this);
 		this.readyToRenderRevenue = true;
 	};
 
@@ -33,6 +34,8 @@ export default class CbPLSummaryDashboard extends LightningElement {
 	};
 
 	///////// REVENUE PART ///////////
+	@track readyToRenderRevenue = false;
+	@track isRevenueOptionsRendered = false;
 	@track groupRevenue = false;
 	@track revenueCurrentPlanActualPercentDiffConfig;
 	@track revenueCurrentPreviousMonthPercentDiffConfig;
@@ -40,11 +43,8 @@ export default class CbPLSummaryDashboard extends LightningElement {
 	@track revenueBudgetActualConfig;
 	@track revenueOptions;
 	@track revenueValues;
-
-	handleRevenueOptionsChange = (event) => {
-		this.revenueValues = event.detail.value;
-	};
-
+	timeoutId;
+	handleRevenueOptionsChange = (event) => this.revenueValues = event.detail.value;
 	applyRevenueOptions = () => {
 		this.readyToRenderRevenue = false;
 		this.isRevenueOptionsRendered = false;
@@ -53,27 +53,45 @@ export default class CbPLSummaryDashboard extends LightningElement {
 			this.readyToRenderRevenue = true;
 		}, 500);
 	};
-
 	invertRevenueOptions = () => this.revenueValues = this.revenueOptions.filter(so => !this.revenueValues.includes(so.value)).map(so => so.value);
-
-	allRevenueOptions = () => {
-		this.revenueValues = this.revenueOptions.map(so => so.value);
-	};
-
-	@track isRevenueOptionsRendered = false;
-	timeoutId;
+	allRevenueOptions = () => this.revenueValues = this.revenueOptions.map(so => so.value);
 
 	handleMouseOverRevenue() {
 		clearTimeout(this.timeoutId);
 		this.isRevenueOptionsRendered = true;
 	}
 
-	handleMouseOutRevenue() {
-		this.timeoutId = setTimeout(() => {
-			this.isRevenueOptionsRendered = false;
-		}, 1000);
+	handleMouseOutRevenue = () => this.timeoutId = setTimeout(() => this.isRevenueOptionsRendered = false, 1000);
+	///////// REVENUE PART ///////////
+
+	///////// COGS PART ///////////
+	@track readyToRenderCOGS = false;
+	@track isCOGSOptionsRendered = false;
+	@track groupCOGS = false;
+	@track COGSCurrentPlanActualPercentDiffConfig;
+	@track COGSCurrentPreviousMonthPercentDiffConfig;
+	@track COGSCurrentPreviousYearPercentDiffConfig;
+	@track COGSBudgetActualConfig;
+	@track COGSOptions;
+	@track COGSValues;
+	handleCOGSOptionsChange = (event) => this.COGSValues = event.detail.value;
+	applyCOGSOptions = () => {
+		this.readyToRenderCOGS = false;
+		this.isCOGSOptionsRendered = false;
+		setTimeout(() => {
+			prepareCOGS(this);
+			this.readyToRenderCOGS = true;
+		}, 500);
+	};
+	invertCOGSOptions = () => this.COGSValues = this.COGSOptions.filter(so => !this.COGSValues.includes(so.value)).map(so => so.value);
+	allCOGSOptions = () => this.COGSValues = this.COGSOptions.map(so => so.value);
+
+	handleMouseOverCOGS() {
+		clearTimeout(this.timeoutId);
+		this.isCOGSOptionsRendered = true;
 	}
 
-	///////// REVENUE PART ///////////
+	handleMouseOutCOGS = () => this.timeoutId = setTimeout(() => this.isCOGSOptionsRendered = false, 1000);
+	///////// COGS PART ///////////
 
 }
---
+++ src/lwc/cbPLSummaryDashboard/cbPLSummaryDashboardCOGS.js
@@ -0,0 +1,189 @@
+import {_message} from "c/cbUtils";
+
+let c;
+
+const prepareCOGS = (context) => {
+	try {
+		if (context) c = context;
+		c.reportLines.forEach(rl => console.log('ALLRL: ' + JSON.stringify(rl)));
+		let COGSReportLines = c.reportLines.filter(rl => rl.type === 'COGS');
+		console.log('SELECTED COGS:: ' + JSON.stringify(COGSReportLines));
+		c.COGSOptions = getCOGSOptions(COGSReportLines);
+		if (!c.COGSValues || c.COGSValues.length === 0) c.COGSValues = getCOGSValues(COGSReportLines);
+		COGSReportLines = COGSReportLines.filter(rl => c.COGSValues.includes(rl.label));
+		c.COGSCurrentPlanActualPercentDiffConfig = getDataForCOGSBudgetByVar2Chart(COGSReportLines, 'currentMonthDiffPercent', 'Plan/Actual Diff');
+		c.COGSCurrentPreviousMonthPercentDiffConfig = getDataForCOGSBudgetByVar2Chart(COGSReportLines, 'priorMonthDiffPercent', 'Current/Previous Month Diff');
+		c.COGSCurrentPreviousYearPercentDiffConfig = getDataForCOGSBudgetByVar2Chart(COGSReportLines, 'priorYearDiffPercent', 'Current/Previous Year Diff');
+		c.COGSBudgetActualConfig = getDataForCOGSBudgetActualChart(COGSReportLines);
+		c.readyToRenderCOGS = true;
+	} catch (e) {
+		_message('error', 'COGS Chart Error: ' + JSON.stringify(e));
+	}
+};
+
+const getCOGSOptions = (COGSReportLines) => {
+	return COGSReportLines.reduce((so, rl) => {
+		so.push({label: rl.label, value: rl.label});
+		return so;
+	}, []);
+};
+
+const getCOGSValues = (COGSReportLines) => {
+	return COGSReportLines.reduce((values, rl) => {
+		values.push(rl.label);
+		return values;
+	}, []);
+};
+
+const getDataForCOGSBudgetByVar2Chart = (COGSReportLines, field, label) => {
+
+	const labels = [];
+	const data = [];
+	COGSReportLines.forEach(rl => {
+		labels.push(rl.label);
+		data.push(rl[field] * 100);
+	});
+
+	const backgroundColors = generateColors(data.length);
+	const borderColors = backgroundColors.map(color => color.replace('0.2', '1'));
+
+	return {
+		type: 'bar',
+		data: {
+			labels,
+			datasets: [{
+				label,
+				data,
+				backgroundColor: backgroundColors,
+				borderColor: borderColors,
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					title: {
+						display: true,
+						text: 'Percentage'
+					},
+					beginAtZero: true,
+					max: 100, // Ensure Y-axis goes up to 100%
+					ticks: {
+						callback: function (value) {
+							return value + '%'; // Convert to percentage
+						}
+					},
+					scaleLabel: {
+						display: true,
+						labelString: "Percentage"
+					}
+				}
+			},
+			plugins: {
+				tooltip: {
+					callbacks: {
+						label: function (tooltipItem) {
+							return tooltipItem.raw + '%'; // Show percentage in tooltip
+						}
+					}
+				},
+				legend: {
+					display: true, // Enable the legend
+					onClick: (e, legendItem) => {
+						const index = legendItem.index;
+						const chart = legendItem.chart;
+						const meta = chart.getDatasetMeta(0);
+						meta.data[index].hidden = !meta.data[index].hidden;
+						chart.update();
+					}
+				}
+			}
+		}
+	};
+};
+
+const getDataForCOGSBudgetActualChart = (COGSReportLines) => {
+	const labels = ['Current Month', 'Prior Month', 'Prior Year Month'];
+	const actualData = [0, 0, 0];
+	const budgetData = [0, 0, 0];
+
+	COGSReportLines.forEach(rl => {
+		actualData[0] += rl.currentMonthActual;
+		actualData[1] += rl.priorMonthActual;
+		actualData[2] += rl.priorYearActual;
+		budgetData[0] += rl.currentMonthBudget;
+		budgetData[1] += rl.priorMonthBudget;
+		budgetData[2] += rl.priorYearBudget;
+	});
+
+	const actualDataset = {
+		label: 'Actual',
+		data: actualData,
+		type: 'line',
+		borderColor: 'rgba(255, 99, 132, 1)',
+		fill: false,
+		borderWidth: 2,
+		tension: 0
+	};
+	const budgetDataset = {
+		label: 'Budget',
+		data: budgetData,
+		backgroundColor: 'rgba(54, 162, 235, 0.2)',
+		borderColor: 'rgba(54, 162, 235, 1)',
+		borderWidth: 1
+	};
+
+	return {
+		type: 'bar',
+		data: {
+			labels,
+			datasets: [actualDataset, budgetDataset]
+		},
+		options: {
+			scales: {
+				y: {
+					ticks: {
+						// Include a dollar sign in the ticks
+						callback: (value, index, ticks) => {
+							return '$' + value;
+						}
+					},
+					scaleLabel: {
+						display: true,
+						labelString: 'Amount ($)'
+					},
+					title: {
+						display: true,
+						text: 'Amount ($)',
+					}
+				}
+			},
+			plugins: {
+				title: {
+					display: true,
+					text: 'COGS Budget vs Actual'
+				}
+			}
+		}
+	};
+};
+
+// Method to generate an array of colors
+const generateColors = (numColors) => {
+	const colors = [];
+	for (let i = 0; i < numColors; i++) {
+		const r = Math.floor(Math.random() * 255);
+		const g = Math.floor(Math.random() * 255);
+		const b = Math.floor(Math.random() * 255);
+		colors.push(`rgba(${r}, ${g}, ${b}, 0.2)`);
+	}
+	return colors;
+};
+
+const resetCOGS = () => {
+	c.COGSCurrentPlanActualPercentDiffConfig = [];
+	c.COGSCurrentPreviousMonthPercentDiffConfig = [];
+	c.COGSCurrentPreviousYearPercentDiffConfig = [];
+};
+
+export {prepareCOGS}
